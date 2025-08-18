83cd849da01473f30f4de54841ee0a2d1e833d3b
NogaHaimovich <noga.haimovich153@gmail.com>
2025-08-18 20:27:51 +0300

    add companies logos

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -2,6 +2,10 @@
 import "./styles.scss";
 import Star from '../../assets/star.svg?react';
 import Heart from '../../assets/heart-2.svg?react';
+import Wolt from "../../assets/wolt.svg?react"
+import Cibus from "../../assets/cibus.svg?react"
+import Tenbis from "../../assets/tenbis.svg?react"
+import Tabit from "../../assets/tabit.svg?react"
 
 const Card = () => {
   return (
@@ -9,6 +13,12 @@ const Card = () => {
         <div className="card__image"></div>
         <div className="card__detailes">
             <h1>שם המסעדה</h1>
+            <div className="card__companies">      
+                <Cibus data-testid="cibus-logo" className="company-logo" />
+                <Tabit data-testid="tabit-logo" className="company-logo"/>
+                <Tenbis data-testid="tenbis-logo" className="company-logo"/>
+                <Wolt data-testid="wolt-logo" className="company-logo"/>
+            </div>
             <div className="card__detailes-info">
                 <div className="card__detailes-info-rate">
                     <h2>3.4</h2>
